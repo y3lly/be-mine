cdb7f67463db1340994f2096da45628bf4bbc510
Mariel Jenine <155594590+y3lly@users.noreply.github.com>
2024-04-27 19:27:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "I love you";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
